2a82a985f67e09d950aa9a694e620a50cd421818
Angel Caro <angelcaro@angels-macbook-pro.local>
2016-04-13 09:36:50 -0600

    Added toolbars, buttons, imageView, camera and album functions and keyboard functions.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var pickerImageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
 
+    //MARK: System methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        //Check for camera availability.
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        
+        
+        //Set top and bottom label text
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+     
+        topTextField.clearsOnBeginEditing = true
+        bottomTextField.clearsOnBeginEditing = true
+        
+    }
+    
+   
+    //Hide status bar
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Functions
+    func imagePickerController(picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        pickerImageView.image = image
+    }
+    
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
     }
 
+    //Select Camera
+    @IBAction func cameraButton(sender: UIBarButtonItem) {
+        
+        let camera = UIImagePickerController()
+        camera.delegate = self
+        camera.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        presentViewController(camera, animated: true, completion: nil)
+        
+    }
+   
+    //Select Photo from album
+    @IBAction func albumButton(sender: AnyObject) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+        
+    }
 
+    //Share image
+    @IBAction func shareButton(sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    //Cancel Meme
+    @IBAction func cancelButton(sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    //TextField functions
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
+        self.view.endEditing(true)
+        return false
+        
+    }
+    
+
+    
+  
 }
 
